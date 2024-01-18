177064b1078606070b6528ba89d18d45c66e8001
ZarkWang <zijianwang1996@gmail.com>
2024-01-18 21:18:26 +0000

    blank-filling questions

--- stimuli-timeline.js
+++ stimuli-timeline.js
@@ -1,61 +1,48 @@
 
-import { rand,addCheckboxes } from "./lib/utils.js";
+import { rand } from "./lib/utils.js";
 
 
-const amount_frontVary = [
-    {"front_amount": 60, "backend_amount": 60},
-    {"front_amount": 120, "backend_amount": 60},
-    {"front_amount": 180, "backend_amount": 60},
-    {"front_amount": 240, "backend_amount": 60},
-    {"front_amount": 300, "backend_amount": 60},
-    {"front_amount": 360, "backend_amount": 60},
-    {"front_amount": 420, "backend_amount": 60},
-    {"front_amount": 480, "backend_amount": 60},
-    {"front_amount": 540, "backend_amount": 60},
-    {"front_amount": 600, "backend_amount": 60},
-    {"front_amount": 660, "backend_amount": 60},
-    {"front_amount": 720, "backend_amount": 60},
-    {"front_amount": 780, "backend_amount": 60},
-    {"front_amount": 840, "backend_amount": 60},
-    ]
-
-// (4+4)
-
-const amount_backVary = [
-    {"front_amount": 40, "backend_amount": 80},
-    {"front_amount": 40, "backend_amount": 120},
-    {"front_amount": 40, "backend_amount": 160},
-    {"front_amount": 40, "backend_amount": 200},
-    {"front_amount": 80, "backend_amount": 80},
-    {"front_amount": 80, "backend_amount": 120},
-    {"front_amount": 80, "backend_amount": 160},
-    {"front_amount": 80, "backend_amount": 200},
-    ]
-
-// (4+4)
+// Generate stimuli for main tasks
+const stimuliNumber = 14;
 
 const seqLengthList = ['6 months','12 months'];
 
-// possible stimuli in total: (8 + 8) * 2 = 32
+const amountVaryFront = [
+    {"front_amount": 25, "backend_amount": 60},
+    {"front_amount": 105, "backend_amount": 60},
+    {"front_amount": 185, "backend_amount": 60},
+    {"front_amount": 265, "backend_amount": 60},
+    {"front_amount": 345, "backend_amount": 60},
+    {"front_amount": 425, "backend_amount": 60},
+    {"front_amount": 505, "backend_amount": 60},
+    ]
 
-const stimuliContext = {
-    'n': 14, // number of stimuli
-    'stimuli_frontAlign': fullStimuli('front-align',amount_frontVary,seqLengthList),
-    'stimuli_backAlign': fullStimuli('back-align',amount_backVary,seqLengthList)
-}
+
+const fullStimuli = configAllStimuli(amountVaryFront,seqLengthList)
+
+const drawnStimuli = drawRandStimuli(stimuliNumber, fullStimuli)
+
+// Generate stimuli for consistency check tasks
+const amountForCheck = [
+    {"front_amount": 25, "backend_amount": 60, 'single_amount': 100},
+    {"front_amount": 185, "backend_amount": 60,'single_amount': 300},
+    {"front_amount": 345, "backend_amount": 60,'single_amount': 345},
+    {"front_amount": 505, "backend_amount": 60,'single_amount': 505},
+    ]
 
 
-// Generate the full stimuli array
-function fullStimuli(conditionName,amountArray,seqLengthList){
+
+// Function to generate the full stimuli array
+function configAllStimuli(amountArray,seqLengthList){
     let stimuliArray = [];
     let i, j;
 
-    for(i = 0; i < amountArray.length; i++){
-        for(j = 0; j < seqLengthList.length; j++){
-            let stimulus = {'condition': conditionName};
-            stimulus['q_id'] = i*seqLengthList.length + j;
-            stimulus['seq_length'] = seqLengthList[j];
-            stimulus = Object.assign({}, stimulus, amountArray[i]);
+    for(i = 0; i < seqLengthList.length; i++){
+        for(j = 0; j < amountArray.length; j++){
+            let stimulus = {};
+            stimulus['q_id'] = i+ j*seqLengthList.length;
+            stimulus['seq_length'] = seqLengthList[i];
+            stimulus = Object.assign({}, stimulus, amountArray[j]);
             stimuliArray.push(stimulus);
         }
     }
@@ -63,42 +50,13 @@ function fullStimuli(conditionName,amountArray,seqLengthList){
 };
 
 
-// Randomly draw stimuli from each stimuli array
-function drawStimuli(stimuliArray,n,seed){
+// Function to randomly draw stimuli from the full stimuli array
+function drawRandStimuli(stimuliNumber,stimuliArray){
 
-    let qID = rand(n,stimuliArray.length,seed);
+    let qID = rand(stimuliNumber,stimuliArray.length-1);
     let stimuliDrawn = qID.map(qID => stimuliArray[qID]);
     return(stimuliDrawn)
 };
 
 
-// Load current stimulus by index
-function loadStimuli(stimuli,index){
-
-    let qOrder = Math.floor(index/2);
-    let presentStimulus = stimuli[qOrder];
-    presentStimulus['present_order'] = qOrder;
-
-    return(presentStimulus)
-};
-
-
-// Stimuli for practice tasks
-const practiceStimuli = {
-    'front-align':[
-        {"train_id":0, "front_amount": 600, "backend_amount": 180, "seq_length": "12 months", "condition": "front-align"},
-        // {"train_id":1, "front_amount": 30, "backend_amount": 90, "seq_length": "1 month", "condition": "front-align"},
-    ],
-    'back-align':[
-        {"train_id":0, "front_amount": 400, "backend_amount": 100, "seq_length": "3 months", "condition": "back-align"},
-        {"train_id":1, "front_amount": 20, "backend_amount": 200, "seq_length": "6 months", "condition": "back-align"},
-    ]
-}
-
-// Lotteris
-const lotteries = [{'outcome':300, 'prob':'50%'},
-                   {'outcome':600, 'prob':'50%'},
-                   {'outcome':900, 'prob':'50%'},
-]
-
-export {stimuliContext,practiceStimuli,lotteries,rand,drawStimuli,loadStimuli,addCheckboxes}
+export {drawnStimuli,amountForCheck,seqLengthList}
